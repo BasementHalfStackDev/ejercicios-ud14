a7932f745967b474f50b374e858a014a47b75ca9
Joan <joan.lapeyra@estudiantat.upc.edu>
2023-03-30 12:11:44 +0200

    tabulació

--- Ejercicio_12_Cursps.sql
+++ Ejercicio_12_Cursps.sql
@@ -2,35 +2,34 @@ create database ud14_ej_12;
 use ud14_ej_12;
 
 create table Profesores (
-	dni char(9) primary key,
+	dni varchar(9) primary key,
 	nombre varchar(40),
 	apellido1 varchar(40),
     apellido2 varchar(40),
 	direccion varchar(80),
 	titulo varchar(40),
-	gana decimal(10, 2),
-    unique (nombre, apellido1, apellido2)
+	gana decimal(10, 2) not null,
+	unique (nombre, apellido1, apellido2)
 );
-
 
 create table Cursos (
 	codigo int primary key,
 	nombre varchar(40) unique,
 	dni_profesor char(9) not null references Profesores on update cascade,
 	maximo_alumnos int,
-    fecha_inicio date,
+	fecha_inicio date,
 	fecha_fin date,
 	num_horas int not null,
 	check (fecha_inicio < fecha_fin)
 );
 
 create table Alumnos(
-	dni char(9) primary key,
+	dni varchar(9) primary key,
 	nombre varchar(40),
 	apellido1 varchar(40),
-    apellido2 varchar(40),
+	apellido2 varchar(40),
 	direccion varchar(80),
 	sexo enum('M', 'H'),
-    fecha_nacimiento date,
+	fecha_nacimiento date,
 	curso int not null references Cursos on update cascade
 );
